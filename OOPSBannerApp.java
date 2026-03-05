409057a5536b68e0b322b4d5fed91b44b41d8ca9
nightsky-tech <astrokrithin@gmail.com>
2026-03-05 22:04:05 +0530

    UC8: Implement HashMap to store character patterns and render banner using functions

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,74 +1,67 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class OOPSBannerApp {
 
-    // Function to generate line for letter O
-    public static String getO(int line) {
-        switch (line) {
-            case 0:
-            case 6:
-                return " ***** ";
-            case 1:
-            case 2:
-            case 3:
-            case 4:
-            case 5:
-                return "*     *";
-            default:
-                return "";
-        }
+    // Function to build character pattern map
+    public static Map<Character, String[]> buildPatternMap() {
+
+        Map<Character, String[]> patternMap = new HashMap<>();
+
+        patternMap.put('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        });
+
+        patternMap.put('P', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                "*      ",
+                "*      ",
+                "*      "
+        });
+
+        patternMap.put('S', new String[]{
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        });
+
+        return patternMap;
     }
 
-    // Function to generate line for letter P
-    public static String getP(int line) {
-        switch (line) {
-            case 0:
-                return " ***** ";
-            case 1:
-            case 2:
-                return "*     *";
-            case 3:
-                return " ***** ";
-            case 4:
-            case 5:
-            case 6:
-                return "*      ";
-            default:
-                return "";
-        }
-    }
+    // Function to render banner
+    public static void renderBanner(String word, Map<Character, String[]> patternMap) {
 
-    // Function to generate line for letter S
-    public static String getS(int line) {
-        switch (line) {
-            case 0:
-            case 6:
-                return " ***** ";
-            case 1:
-            case 2:
-                return "*      ";
-            case 3:
-                return " ***** ";
-            case 4:
-            case 5:
-                return "      *";
-            default:
-                return "";
+        for (int i = 0; i < 7; i++) {
+            StringBuilder line = new StringBuilder();
+
+            for (char c : word.toCharArray()) {
+                String[] pattern = patternMap.get(c);
+                line.append(pattern[i]).append("    ");
+            }
+
+            System.out.println(line);
         }
     }
 
     public static void main(String[] args) {
 
-        String[] banner = new String[7];
+        Map<Character, String[]> patternMap = buildPatternMap();
 
-        for (int i = 0; i < 7; i++) {
-            banner[i] = String.join("    ",
-                    getO(i),
-                    getO(i),
-                    getP(i),
-                    getS(i));
-        }
+        String word = "OOPS";
 
-        for (String line : banner) {
-            System.out.println(line);
-        }
+        renderBanner(word, patternMap);
     }
 }
